a3abe43769709c92a0dc73fb1cc0706766ce3fbd
Pavel Generalov <generalovpn@yandex.ru>
2023-01-22 13:29:03 +0300

    добавить проектную работу №4

--- vendor/scripts/index.js
+++ vendor/scripts/index.js
@@ -1,15 +1,20 @@
+let profileBtnEdit = document.querySelector('.profile__batton-edit');
 let popup = document.querySelector('.popup');
-let profileButtonEdit = document.querySelector('.profile__batton-edit');
-let popupButtonClose = document.querySelector('.popup__button-close');
-let elementHeart = document.querySelectorAll('.element__heart');
+let popupBtnClose = document.querySelector('.popup__button-close');
 let popupBtnSave = document.querySelector('.popup__btn-save');
+let formElement = document.querySelector('form');
+let nameInput = document.querySelector('.popup__name');
+let jobInput = document.querySelector('.popup__about-me');
+let profileName = document.querySelector('.profile__name');
+let profileAboutMe = document.querySelector('.profile__about-me');
+let elementHeart = document.querySelectorAll('.element__heart');
 
-profileButtonEdit.addEventListener('click', openPopup);
-popupButtonClose.addEventListener('click', closePopup);
+
+profileBtnEdit.addEventListener('click', openPopup);
+popupBtnClose.addEventListener('click', closePopup);
 
 function openPopup () {
   popup.classList.add('popup_opened');
-
   nameInput.value = profileName.textContent;
   jobInput.value = profileAboutMe.textContent;
 }
@@ -18,29 +23,14 @@ function closePopup(){
   popup.classList.remove('popup_opened')
 }
 
-popup.addEventListener('click', function(event) {
-  if (event.target === event.currentTarget) {
-    closePopup()
-  }
-});
-
-
 elementHeart.forEach(function (elementHeart) {
   elementHeart.addEventListener('click', function() {
     elementHeart.classList.toggle('active')
   })
 });
 
-let formElement = document.querySelector('form');
-let nameInput = document.querySelector('.popup__name');
-let jobInput = document.querySelector('.popup__about-me');
-
 function handleFormSubmit(evt) {
   evt.preventDefault();
-
-  let profileName = document.querySelector('.profile__name');
-  let profileAboutMe = document.querySelector('.profile__about-me');
-
   profileName.textContent = nameInput.value;
   profileAboutMe.textContent = jobInput.value;
 }
