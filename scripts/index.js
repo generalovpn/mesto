75a6e55d65edfe133d3d3ebe69f18dc44592d8fb
Pavel Generalov <generalovpn@yandex.ru>
2023-01-27 20:04:41 +0300

    fix: исправить замечания ревьюера

---
+++ scripts/index.js
@@ -0,0 +1,38 @@
+let profileBtnEdit = document.querySelector('.profile__btn-edit');
+let popup = document.querySelector('.popup');
+let popupBtnClose = document.querySelector('.popup__btn-close');
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__field_type_name');
+let jobInput = document.querySelector('.popup__field_type_profession');
+let profileName = document.querySelector('.profile__name');
+let profileAboutMe = document.querySelector('.profile__about-me');
+// let elementHeart = document.querySelectorAll('.group__heart');
+
+profileBtnEdit.addEventListener('click', openPopup);
+popupBtnClose.addEventListener('click', closePopup);
+
+function openPopup () {
+  popup.classList.add('popup_opened');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileAboutMe.textContent;
+}
+
+function closePopup(){
+  popup.classList.remove('popup_opened')
+}
+
+// elementHeart.forEach(function (elementHeart) {
+//   elementHeart.addEventListener('click', function() {
+//     elementHeart.classList.toggle('active')
+//   })
+// });
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileAboutMe.textContent = jobInput.value;
+  closePopup();
+}
+
+formElement.addEventListener('submit', handleFormSubmit);
+
